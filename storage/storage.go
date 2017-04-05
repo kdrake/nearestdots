67ed46a8e58c3173eeec543116ae928428942748
kdrake <konstantindrake@gmail.com>
2017-04-05 13:23:16 +0500

    make storage thread-safe

--- storage/storage.go
+++ storage/storage.go
@@ -1,10 +1,12 @@
 package storage
 
 import (
-	"errors"
-	"math"
+	"sync"
+	"time"
 
 	"github.com/dhconnelly/rtreego"
+	"github.com/kdrake/nearestdots/storage/lru"
+	"github.com/pkg/errors"
 )
 
 type (
@@ -17,8 +19,18 @@ type (
 	Driver struct {
 		ID           int
 		LastLocation Location
+		Expiration   int64
+		Locations    *lru.LRU
 	}
 )
+
+// Expired return true if the item has expired
+func (d *Driver) Expired() bool {
+	if d.Expiration == 0 {
+		return false
+	}
+	return time.Now().UnixNano() > d.Expiration
+}
 
 // Bounds method needs for correct working of rtree
 // Lat - Y, Lon - X on coordinate system
@@ -31,43 +43,68 @@ var ErrDriverDoesNotExist = errors.New("Driver does not exist")
 
 // DriverStorage is main storage for our project
 type DriverStorage struct {
+	mu        *sync.RWMutex
 	drivers   map[int]*Driver
 	locations *rtreego.Rtree
+	lruSize   int
 }
 
 // New creates new instance of DriverStorage
-func New() *DriverStorage {
-	d := &DriverStorage{}
-	d.drivers = make(map[int]*Driver)
-	d.locations = rtreego.NewTree(2, 25, 50)
-	return d
+func New(lruSize int) *DriverStorage {
+	s := new(DriverStorage)
+	s.drivers = make(map[int]*Driver)
+	s.locations = rtreego.NewTree(2, 25, 50)
+	s.mu = new(sync.RWMutex)
+	s.lruSize = lruSize
+	return s
 }
 
-// Set sets driver to storage by key
-func (d *DriverStorage) Set(key int, driver *Driver) {
-	_, ok := d.drivers[key]
+// Set an Driver to the storage, replacing any existing item.
+func (s *DriverStorage) Set(driver *Driver) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	d, ok := s.drivers[driver.ID]
 	if !ok {
-		d.locations.Insert(driver)
+		d = driver
+		cache, err := lru.New(s.lruSize)
+		if err != nil {
+			return errors.Wrap(err, "could not create LRU")
+		}
+		d.Locations = cache
+		s.locations.Insert(d)
 	}
-	d.drivers[key] = driver
+	d.LastLocation = driver.LastLocation
+	d.Locations.Add(time.Now().UnixNano(), d.LastLocation)
+	d.Expiration = driver.Expiration
+
+	s.drivers[driver.ID] = driver
+	return nil
 }
 
-// Delete removes driver from storage by key
-func (d *DriverStorage) Delete(key int) error {
-	driver, ok := d.drivers[key]
+// Delete deletes a driver from storage. Does nothing if the driver is not in the storage.
+func (s *DriverStorage) Delete(id int) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	driver, ok := s.drivers[id]
 	if !ok {
 		return ErrDriverDoesNotExist
 	}
-	if d.locations.Delete(driver) {
-		delete(d.drivers, key)
+	deleted := s.locations.Delete(driver)
+	if deleted {
+		delete(s.drivers, driver.ID)
 		return nil
 	}
-	return errors.New("could not remove driver")
+	return errors.New("could not remove item")
 }
 
 // Get gets driver from storage and an error if nothing found
-func (d *DriverStorage) Get(key int) (*Driver, error) {
-	driver, ok := d.drivers[key]
+func (s *DriverStorage) Get(id int) (*Driver, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	driver, ok := s.drivers[id]
 	if !ok {
 		return nil, ErrDriverDoesNotExist
 	}
@@ -75,9 +112,11 @@ func (d *DriverStorage) Get(key int) (*Driver, error) {
 }
 
 // Nearest returns nearest drivers by location
-func (d *DriverStorage) Nearest(count int, lat, lon float64) []*Driver {
-	point := rtreego.Point{lat, lon}
-	results := d.locations.NearestNeighbors(count, point)
+func (s *DriverStorage) Nearest(point rtreego.Point, count int) []*Driver {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	results := s.locations.NearestNeighbors(count, point)
 	var drivers []*Driver
 	for _, item := range results {
 		if item == nil {
@@ -88,30 +127,17 @@ func (d *DriverStorage) Nearest(count int, lat, lon float64) []*Driver {
 	return drivers
 }
 
-// haversin(θ) function
-func hsin(theta float64) float64 {
-	return math.Pow(math.Sin(theta/2), 2)
-}
+// DeleteExpired removes all expired items from storage
+func (s *DriverStorage) DeleteExpired() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 
-// Distance function returns the distance (in meters) between two points of
-//     a given longitude and latitude relatively accurately (using a spherical
-//     approximation of the Earth) through the Haversin Distance Formula for
-//     great arc distance on a sphere with accuracy for small distances
-//
-// point coordinates are supplied in degrees and converted into rad. in the func
-//
-// distance returned is METERS!!!!!!
-// http://en.wikipedia.org/wiki/Haversine_formula
-func Distance(lat1, lon1, lat2, lon2 float64) float64 {
-	// convert to radians
-	// must cast radius as float to multiply later
-	var la1, lo1, la2, lo2, r float64
-	la1 = lat1 * math.Pi / 180
-	lo1 = lon1 * math.Pi / 180
-	la2 = lat2 * math.Pi / 180
-	lo2 = lon2 * math.Pi / 180
-	r = 6378100 // Earth radius in METERS
-	// calculate
-	h := hsin(la2-la1) + math.Cos(la1)*math.Cos(la2)*hsin(lo2-lo1)
-	return 2 * r * math.Asin(math.Sqrt(h))
+	for _, d := range s.drivers {
+		if d.Expired() {
+			deleted := s.locations.Delete(d)
+			if deleted {
+				delete(s.drivers, d.ID)
+			}
+		}
+	}
 }
--- storage/storage_test.go
+++ storage/storage_test.go
@@ -2,56 +2,88 @@ package storage
 
 import (
 	"testing"
+	"time"
 
+	"github.com/dhconnelly/rtreego"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestStorage(t *testing.T) {
-	s := New()
-	driver := &Driver{
-		ID: 1,
-		LastLocation: Location{
-			Lat: 1,
-			Lon: 1,
-		},
-	}
-	s.Set(driver.ID, driver)
-
-	d, err := s.Get(driver.ID)
-	assert.NoError(t, err)
-	assert.Equal(t, d, driver)
-
-	err = s.Delete(driver.ID)
-	assert.NoError(t, err)
-
-	d, err = s.Get(driver.ID)
-	assert.Equal(t, err, ErrDriverDoesNotExist)
-}
-
-func TestNearest(t *testing.T) {
-	s := New()
-	s.Set(123, &Driver{
+func TestDriverStorage(t *testing.T) {
+	s := New(10)
+	s.Set(&Driver{
 		ID: 123,
 		LastLocation: Location{
 			Lat: 1,
 			Lon: 1,
 		},
+		Expiration: time.Now().Add(15).UnixNano(),
 	})
-	s.Set(666, &Driver{
-		ID: 666,
+
+	d, err := s.Get(123)
+	assert.NoError(t, err)
+	assert.Equal(t, d.ID, 123)
+
+	err = s.Delete(123)
+	assert.NoError(t, err)
+
+	d, err = s.Get(123)
+	assert.Equal(t, ErrDriverDoesNotExist, err)
+}
+
+func TestNearest(t *testing.T) {
+	s := New(10)
+	s.Set(&Driver{
+		ID: 123,
 		LastLocation: Location{
 			Lat: 42.875799,
 			Lon: 74.588279,
 		},
+		Expiration: time.Now().Add(15).UnixNano(),
 	})
-	drivers := s.Nearest(1, 42.876420, 74.588332)
-	assert.Equal(t, 1, len(drivers))
+	s.Set(&Driver{
+		ID: 321,
+		LastLocation: Location{
+			Lat: 42.875508,
+			Lon: 74.588107,
+		},
+		Expiration: time.Now().Add(15).UnixNano(),
+	})
+	s.Set(&Driver{
+		ID: 666,
+		LastLocation: Location{
+			Lat: 42.876106,
+			Lon: 74.588204,
+		},
+		Expiration: time.Now().Add(15).UnixNano(),
+	})
+	s.Set(&Driver{
+		ID: 2319,
+		LastLocation: Location{
+			Lat: 42.874942,
+			Lon: 74.585908,
+		},
+		Expiration: time.Now().Add(15).UnixNano(),
+	})
+	s.Set(&Driver{
+		ID: 991,
+		LastLocation: Location{
+			Lat: 42.875744,
+			Lon: 74.584503,
+		},
+		Expiration: time.Now().Add(15).UnixNano(),
+	})
+
+	drivers := s.Nearest(rtreego.Point{42.876420, 74.588332}, 3)
+	assert.Equal(t, len(drivers), 3)
+	assert.Equal(t, drivers[0].ID, 123)
+	assert.Equal(t, drivers[1].ID, 321)
+	assert.Equal(t, drivers[2].ID, 666)
 }
 
 func BenchmarkNearest(b *testing.B) {
-	s := New()
+	s := New(100)
 	for i := 0; i < 100; i++ {
-		s.Set(i, &Driver{
+		s.Set(&Driver{
 			ID: i,
 			LastLocation: Location{
 				Lat: float64(i),
@@ -59,7 +91,25 @@ func BenchmarkNearest(b *testing.B) {
 			},
 		})
 	}
+	point := rtreego.Point{123, 123}
 	for i := 0; i < b.N; i++ {
-		s.Nearest(10, 123, 123)
+		s.Nearest(point, 10)
 	}
+}
+
+func TestExpire(t *testing.T) {
+	s := New(10)
+	driver := &Driver{
+		ID: 123,
+		LastLocation: Location{
+			Lat: 42.876420,
+			Lon: 74.588332,
+		},
+		Expiration: time.Now().Add(-15).UnixNano(),
+	}
+	s.Set(driver)
+	s.DeleteExpired()
+	d, err := s.Get(123)
+	assert.Error(t, err)
+	assert.NotEqual(t, d, driver)
 }
